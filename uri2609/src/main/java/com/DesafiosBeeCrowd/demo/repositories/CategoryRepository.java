e588c9750584f4f8544d16ecb8200e62dbb71828
devrafaelrabelo <devrafaelrabelo@gmail.com>
2025-02-18 10:14:16 -0300

    URI 2609 - Completo(Corrigido)

--- uri2609/src/main/java/com/DesafiosBeeCrowd/demo/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/DesafiosBeeCrowd/demo/repositories/CategoryRepository.java
@@ -10,14 +10,14 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT categories.name, SUM(products.amount) FROM products " +
-            "INNER JOIN categories ON products.id_categories = categories.id " +
+    @Query(nativeQuery = true, value = "SELECT categories.name, SUM(products.amount) " +
+            "FROM categories " +
+            "INNER JOIN products ON products.id_categories = categories.id " +
             "GROUP BY categories.name ")
     List<CategorySumProjection> search1();
 
-    @Query("SELECT new com.DesafiosBeeCrowd.demo.dto.CategorySumDTO(c.name, SUM(obj.amount)) " +
+    @Query("SELECT new com.DesafiosBeeCrowd.demo.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) " +
             "FROM Product obj " +
-            "JOIN obj.category c " +
-            "GROUP BY c.name ")
+            "GROUP BY obj.category.name ")
     List<CategorySumDTO> search2();
 }
